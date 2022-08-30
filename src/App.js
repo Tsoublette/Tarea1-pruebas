75bfae314f7b1b0ee86573c3e765c644eff6d421
tsoublette <tsoublette@uc.cl>
2022-08-30 15:12:13 -0400

    heroku =

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 
 function App() {
 
-let client = new WebSocket("wss://tarea-1.2022-2.tallerdeintegracion.cl/connect");
+const client = new WebSocket("wss://tarea-1.2022-2.tallerdeintegracion.cl/connect");
 client.onopen = function(e) {
 console.log("[open] Connection established");
 client.send('{"type": "join","id": "15b5c75e-d857-4545-8239-13c9161c97f3","username": "Tomás"}');
+};
 
 client.onmessage = function(event) {
 var mensaje = JSON.parse(event.data);
-console.log(event.data);
+//console.log(event.data);
 if (mensaje["type"]=== "message"){
     console.log(mensaje["message"]["content"])
   }
 };
 
-};
+
   return (
     <div className="App">
       <header className="App-header">
